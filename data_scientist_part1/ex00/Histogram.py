567879782050137890985009a75dfb8bcf91be23
Benjamin Larger <benjaminlarger.bl@gmail.com>
2025-04-23 15:12:01 +0200

    ex00 done

--- data_scientist_part1/ex00/Histogram.py
+++ data_scientist_part1/ex00/Histogram.py
@@ -13,8 +13,8 @@ class Histogram:
     self.table = 'items'
     if not os.path.exists(self.csv_dir):
         os.makedirs(self.csv_dir)
-    self.filename1 = "Test_knight.csv"
-    self.filename2 = "Test_knight2.csv"
+    self.filename_test = "Test_knight.csv"
+    self.filename_train = "Train_knight.csv"
 
   def connect_to_postgres(self):
     conn = psycopg2.connect(
@@ -33,7 +33,7 @@ class Histogram:
         self.conn.close()
     print("PostgreSQL connection closed.")
 
-  def plot_distribution(self, df):
+  def plot_test_distribution(self, df):
       numeric_cols = [col for col in df.columns if pd.api.types.is_numeric_dtype(df[col])]
       n = len(numeric_cols)
 
@@ -44,14 +44,12 @@ class Histogram:
       axes = axes.flatten()
 
       for i, col in enumerate(numeric_cols):
-          # cast as int if dispersion
-
           data = df[col].dropna()
           bin_edges = np.histogram_bin_edges(data, bins='auto')
           hist, edges = np.histogram(data, bins=bin_edges)
 
           bin_centers = 0.5 * (edges[1:] + edges[:-1])
-          axes[i].bar(bin_centers, hist, width=(edges[1] - edges[0]), align='center', color="lightgreen", label="Knight")  # changed from plot to bar
+          axes[i].bar(bin_centers, hist, width=(edges[1] - edges[0]), align='center', color="lightgreen", label="Knight")
           axes[i].set_title(col)
           axes[i].legend()
 
@@ -62,13 +60,48 @@ class Histogram:
       plt.tight_layout()
       plt.show()
   
+  def plot_test_and_train_distribution(self, df_test, df_train):
+      numeric_cols_test = [col for col in df_test.columns if pd.api.types.is_numeric_dtype(df_test[col])]
+      numeric_cols_train = [col for col in df_train.columns if pd.api.types.is_numeric_dtype(df_train[col])]
+      n_test = len(numeric_cols_test)
+      n_train = len(numeric_cols_train)
 
+
+      if n_test == 0 or n_train == 0 or n_test != n_train:
+          print("No numeric columns to plot, or dataframes have different numeric columns.")
+          return
+      fig, axes = plt.subplots(5, 6, figsize=(5 * 5, 4 * 6))
+      axes = axes.flatten()
+
+      for i, col in enumerate(numeric_cols_test):
+          data_test = df_test[col].dropna()
+          data_train = df_train[col].dropna()
+          bin_edges_test = np.histogram_bin_edges(data_test, bins='auto')
+          bin_edges_train = np.histogram_bin_edges(data_train, bins='auto')
+          hist_test, edges_test = np.histogram(data_test, bins=bin_edges_test)
+          hist_train, edges_train = np.histogram(data_train, bins=bin_edges_train)
+          bin_centers_test = 0.5 * (edges_test[1:] + edges_test[:-1])
+          bin_centers_train = 0.5 * (edges_train[1:] + edges_train[:-1])
+          axes[i].bar(bin_centers_test, hist_test, width=(edges_test[1] - edges_test[0]), align='center', color="lightcoral", label="Jedi")
+          axes[i].bar(bin_centers_train, hist_train, width=(edges_train[1] - edges_train[0]), align='center', color="lightblue", alpha=0.5, label="Sith")
+          axes[i].set_title(col)
+          axes[i].legend()
+
+      # Hide unused subplots
+      for j in range(i + 1, len(axes)):
+          fig.delaxes(axes[j])
+
+      plt.tight_layout()
+      plt.show()
 
   def run(self):
-    filepath = os.path.join(self.csv_dir, self.filename1)
-    df = pd.read_csv(filepath, sep=',')
+    filepath1 = os.path.join(self.csv_dir, self.filename_test)
+    filepath2 = os.path.join(self.csv_dir, self.filename_train)
+    df_test = pd.read_csv(filepath1, sep=',')
+    df_train = pd.read_csv(filepath2, sep=',')
     # Iterate through each column in the DataFrame
-    self.plot_distribution(df)
+    self.plot_test_distribution(df_test)
+    self.plot_test_and_train_distribution(df_test, df_train)
     self.close_connection()
 
 def main():
