04de1a099cacf5f38873529b8f8c2042feff422f
Benjamin LARGER <JD6637@engie.com>
2025-04-26 16:43:39 +0200

    ex06 done

--- data_scientist_part2/ex06/democracy.py
+++ data_scientist_part2/ex06/democracy.py
@@ -2,14 +2,16 @@ import pandas as pd
 import os
 from sklearn import preprocessing
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.metrics import f1_score
 import sys
 from sklearn.neighbors import KNeighborsClassifier
 from statsmodels.tools.tools import add_constant
 from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn import model_selection
+from sklearn.ensemble import VotingClassifier
 
-class KNN:
+class Democracy:
   def __init__(self):
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(cur_dir)
@@ -19,7 +21,7 @@ class KNN:
         os.makedirs(self.csv_dir)
     self.filename_training = sys.argv[1] if len(sys.argv) > 2 else "Training_knight.csv"
     self.filename_test = sys.argv[2] if len(sys.argv) > 2 else "Validation_knight.csv"
-    self.filename_output = "KNN.txt"
+    self.filename_output = "Voting.txt"
     self.filepath_training = os.path.join(self.csv_dir, self.filename_training)
     self.filepath_test = os.path.join(self.csv_dir, self.filename_test)
     self.filepath_output = os.path.join(self.csv_dir, self.filename_output)
@@ -48,7 +50,6 @@ class KNN:
         # Splitting the Dataset
         df_training = pd.read_csv(self.filepath_training, sep=',')
         df_training = self.normalize_df(df_training)
-        #print(f"fd training columns = {df_training.columns}")
         selected_features = self.select_features(df_training)
         print(f"----> selected_features = {selected_features}")
         df_training = df_training[selected_features + ['knight']]
@@ -59,46 +60,54 @@ class KNN:
         df_test = self.normalize_df(df_test)
         df_test = df_test[selected_features + ['knight']]
         X_test = df_test.drop(columns=['knight'])
-        y_test = df_test['knight']
 
-        # Training the model
-        K = []
-        f1_scores = []
+        # Create different classifiers
+        clf1 = KNeighborsClassifier(n_neighbors=6)
+        clf2 = RandomForestClassifier(random_state=42)
+        clf3 = LogisticRegression(random_state=42)
 
-        for k in range(2, 21):
-           clf = KNeighborsClassifier(n_neighbors=k)
-           clf.fit(X_train, y_train)
-           y_pred = clf.predict(X_test)
-           f1_score_value = f1_score(y_test, y_pred, average='weighted')
-           K.append(k)
+        labels = ['KNN', 'Random Forest', 'Logistic Regression']
+        print('5-fold cross validation:\n')
+        for clf, label in zip([clf1, clf2, clf3], labels):
+            scores = model_selection.cross_val_score(clf, X_train, y_train, cv=5, scoring='f1_weighted')
+            print("Accuracy: %0.2f (+/- %0.2f) [%s]"
+             % (scores.mean(), scores.std(), label))
+            
+        # Create a Hard Voting Classifier
+        voting_clf_hard = VotingClassifier(
+           estimators=[(labels[0], clf1),
+                    (labels[1], clf2),
+                    (labels[2], clf3)
+                    ],
+                    voting='hard'
+        )
+        voting_clf_soft = VotingClassifier(
+           estimators=[(labels[0], clf1),
+                    (labels[1], clf2),
+                    (labels[2], clf3)
+                    ],
+                    voting='soft'
+        )
 
-           f1_scores.append(f1_score_value)
-          
-        # Evaluating the model
-        for k, f1 in zip(K, f1_scores):
-            print(f"F1 Score for K={k}: {f1}")
+        labels_new = ['KNN', 'Random Forest', 'Logistic Regression', 'Voting Hard', 'Voting Soft']
 
-        # Plot f1 scores
-        plt.plot(K, f1_scores, marker='o')
-        plt.title('F1 Score vs K')
-        plt.xlabel('K')
-        plt.ylabel('F1 Score')
-        plt.xticks(K)
-        plt.grid()
-        plt.show()
+        for clf, label in zip([clf1, clf2, clf3, voting_clf_hard, voting_clf_soft], labels_new):
+           scores = model_selection.cross_val_score(clf, X_train, y_train, cv=5, scoring='f1_weighted')
+           print("f1 score: %0.2f (+/- %0.2f) [%s]"
+             % (scores.mean(), scores.std(), label))
 
+        y_pred = voting_clf_hard.fit(X_train, y_train).predict(X_test)
         with open(self.filepath_output, 'w') as f:
             for pred in y_pred:
                 if pred == 0:
                     f.write('Sith\n')
                 else:
                     f.write('Jedi\n')
-
     except Exception as e:
       print(f"Error: {e}")
 
 def main():
-  a = KNN()
+  a = Democracy()
   a.run()
 
 if __name__ == "__main__":
